7aa601cc8824dd761cb88f977dbddd3b00ee30f3
cristobal <cristobalgh@gmail.com>
2025-09-24 10:50:50 -0300

    varios

--- HUB12_1_8/HUB12_1_8.ino
+++ HUB12_1_8/HUB12_1_8.ino
@@ -22,16 +22,28 @@
 #define c_pin      7 // C en conector IDC HUB12
 
 uint8_t mensaje[] = {
-0b00000000, 0b00000000, 0b00000001, 0b00000010, 0b00000100, 0b00001001, 0b00010010, 0b00100100, 0b01001001, 0b10110110, 0b00110110, 0b01001001, 0b10010100, 0b00100010, 0b01000001, 0b10000000, 0b01001001, 0b10010010, 0b00100100, 0b01001000, 0b10010000, 0b00100000, 0b01000000, 0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b10000000, 0b01000000, 0b00100000, 0b10010000, 0b01001000, 0b00100100, 0b10010010,  0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b01001001, 0b00100100, 0b00010010, 0b00001001, 0b00000100, 0b00000010, 0b00000001, 0b00000000, 0b00000001, 0b10000010, 0b01000100, 0b00101001, 0b10010010, 0b01101100, 0b01101101, 0b10010010, 0b00000001, 0b00000010, 0b00000100, 0b00001001, 0b00010010, 0b00100100, 0b01001001, 0b10010010, 0b00100100, 0b11011010, 0b11011001, 0b00100100, 0b01010010, 0b10001001, 0b00000100, 0b00000010, 0b00100100, 0b01001000, 0b10010000, 0b00100000, 0b01000000, 0b10000000, 0b00000000, 0b00000000,0b00000001, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b10000000, 0b01000000, 0b00100000, 0b10010000, 0b01001000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000001, 0b00000010, 0b00100100, 0b10010010, 0b01001001, 0b00100100, 0b00010010, 0b00001001, 0b00000101, 0b00000010, 0b00000100, 0b00001001, 0b0001001, 0b1010010, 0b01001001, 0b10110010, 0b10110100, 0b01001000};
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010,
+0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010, 0b10101010
+};
 
 size_t len = sizeof(mensaje) / sizeof(mensaje[0]);
 
-int fila = 0;
-int bit = 0;
-int aux = 0;
+int fila  = 1;
+int bit   = 0;
+int aux   = 0;
 
-void setup() 
-{  
+void setup() {  
   pinMode(oe_pin, OUTPUT);
   pinMode(a_pin, OUTPUT);
   pinMode(b_pin, OUTPUT);
@@ -66,45 +78,45 @@ void rotateLeft(uint8_t *mensaje, size_t length) {
         mensaje[i] = (mensaje[i] << 1) | msb;   // Shift left y colocamos el MSB como LSB
     }
 }
+void eligeFila(bool a,bool b,bool c) {
+  digitalWrite(a_pin,a);
+  digitalWrite(b_pin,b);
+  digitalWrite(c_pin,c);
+}
 
-void loop() 
-{
-
- if(aux >=5000){
+void loop() {
+  if(aux >= 5000) {
   randomizeBits(mensaje, len);
   //rotateLeft(mensaje, len);
   aux = 0;
   }
   digitalWrite(oe_pin, 0); // habilita la matriz, enciende los LEDs
-
-  if(fila == 0) { digitalWrite(a_pin, 0); digitalWrite(b_pin, 0); digitalWrite(c_pin, 0); } // Selectores de fila
-  if(fila == 1) { digitalWrite(a_pin, 1); digitalWrite(b_pin, 1); digitalWrite(c_pin, 1); }
-  if(fila == 2) { digitalWrite(a_pin, 0); digitalWrite(b_pin, 1); digitalWrite(c_pin, 1); }
-  if(fila == 3) { digitalWrite(a_pin, 1); digitalWrite(b_pin, 0); digitalWrite(c_pin, 1); }
-  if(fila == 4) { digitalWrite(a_pin, 0); digitalWrite(b_pin, 0); digitalWrite(c_pin, 1); }
-  if(fila == 5) { digitalWrite(a_pin, 1); digitalWrite(b_pin, 1); digitalWrite(c_pin, 0); }
-  if(fila == 6) { digitalWrite(a_pin, 0); digitalWrite(b_pin, 1); digitalWrite(c_pin, 0); }  
-  if(fila == 7) { digitalWrite(a_pin, 1); digitalWrite(b_pin, 0); digitalWrite(c_pin, 0); } 
   
-  for(int i=0;i<=120;i+=8) // for para recorrer byte por byte del arreglo
-  {     
-    for(int x=7;x>=0;x--) // for para recorrer bit por bit del byte actual
-    {
+  if(fila == 1) {eligeFila(1,1,1);}// Selectores de fila el roden es bien arbitrario y depende del hardware
+  if(fila == 2) {eligeFila(0,1,1);}
+  if(fila == 3) {eligeFila(1,0,1);}
+  if(fila == 4) {eligeFila(0,0,1);}
+  if(fila == 5) {eligeFila(1,1,0);}
+  if(fila == 6) {eligeFila(0,1,0);}
+  if(fila == 7) {eligeFila(1,0,0);}
+  if(fila == 0) {eligeFila(0,0,0);}
+    
+  for(int i=0;i<=120;i+=8) { // for para recorrer byte por byte del arreglo     
+    for(int x=7;x>=0;x--) { // for para recorrer bit por bit del byte actual
       bit = bitRead(mensaje[fila+i],x); // lectura del bit dentro del byte actual
       digitalWrite(dato_pinR, bit);     // escritura del bit en la salida 
       digitalWrite(clk_pin, 1);         // primer movimiento para consolidar el bit
       digitalWrite(clk_pin, 0);         // segundo movimiento para consolidar el bit
       //delay(1);
     }
-    //delay(5);    
+    //delay(100);    
   }
   
   digitalWrite(sclk_pin, 1); // primer movimiento para consolidar el byte
   digitalWrite(sclk_pin, 0); // segundo movimiento para consolidar el byte
   digitalWrite(oe_pin, 1);   // deshabilita la matriz, apaga los LEDs
-  //delay(1); // atenuación, opcional
 
   fila++; // cambio de fila
-  if(fila > 7) fila = 0;
+  if(fila == 8) fila = 0;
   aux++;   
 }
